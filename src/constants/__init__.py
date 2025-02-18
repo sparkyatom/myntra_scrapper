142e863f8d1343433fa3d2aa70f6859cd5b353b3
sparkyatom <sandeepanpodder2002612@gmail.com>
2025-02-18 17:59:59 +0530

    hello

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,3 +1,3 @@
 MONGODB_URL_KEY: str="mongodb+srv://sandeepanpodder2002612:cthaYmWxucqSRWrU@cluster0.wuoxm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-MONGO_DATABASE_NAME: str="MYNTRA-REVIEWS"
+MONGO_DATABASE_NAME: str="myntra-reviews"
 SESSION_PRODUCT_KEY: str="PRODUCT_NAME"
